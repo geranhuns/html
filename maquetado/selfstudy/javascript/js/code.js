818c1a3c800cdf406d3ede2ac7c742bd0cf3fdfe
geranhuns <gera.nuncio@gmail.com>
2023-12-04 14:40:33 -0600

    problem1

---
+++ maquetado/selfstudy/javascript/js/code.js
@@ -0,0 +1,7 @@
+cantidad1 = prompt("Ingresa el monto de tu dinero");
+
+if (cantidad < 0.6) {
+  alert("no te alcanza nada");
+} else if (cantidad >= 2.9) {
+  alert("Puedes comprar un potecito de helado de confites o pote de 1/4 kg");
+}
